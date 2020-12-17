2548283a9ac864d462a63367455736c320b10a66
Steve Kane <kanesteven@gmail.com>
2020-12-17 14:55:30 -0600

    initial work on Royal Game Of Ur

---
+++ Assets/03 Royal Game Of Ur/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace RoyalGameOfUr {
+  public static class Extensions {
+    public static Vector3Int RoundToInt(in Vector3 v) {
+      return new Vector3Int(Mathf.RoundToInt(v.x), Mathf.RoundToInt(v.y), Mathf.RoundToInt(v.z));
+    }
+
+    public static Dictionary<Vector3Int, Tile> FromAuthoredTiles(TileAuthoring[] authoredTiles) {
+      var dict = new Dictionary<Vector3Int, Tile>(authoredTiles.Length);
+      foreach (var authoredTile in authoredTiles) {
+        var position = RoundToInt(authoredTile.transform.position);
+        var tile = new Tile { IsRosette = authoredTile.IsRosette };
+
+        dict.Add(position, tile);
+      }
+      return dict;
+    }
+  }
+}
---
+++ Assets/03 Royal Game Of Ur/PlayerPathAuthoring.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerPathAuthoring : MonoBehaviour {
+  public Color PathColor;
+  public Transform[] Positions;
+  public Vector3 RenderingOffset = Vector3.up;
+  public float RenderingRadius = .25f;
+
+  public void OnDrawGizmos() {
+    Gizmos.color = PathColor;
+
+    for (var i = 0; i < Positions.Length; i++) {
+      Gizmos.DrawSphere(Positions[i].position + RenderingOffset, RenderingRadius);
+    }
+
+    for (var i = 0; i < Positions.Length - 1; i++) {
+      Gizmos.DrawLine(Positions[i].position + RenderingOffset, Positions[i+1].position + RenderingOffset);
+    }
+  }
+}
